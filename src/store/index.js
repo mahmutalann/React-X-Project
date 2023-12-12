1616c69733abeda125af40eaea2b4e5d16509a2b
Mahmut <you@example.com>
2023-12-12 12:24:43 +0300

    6th part

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
-import {configureStore} from "@reduxjs/toolkit";
 import auth from "~/store/auth";
+import modal from "~/store/modal";
+import appearance from "~/store/appearance";
+import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({
 	reducer: {
-		auth
+		auth,
+		modal,
+		appearance,
 	}
 })
 
